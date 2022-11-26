f6b8a06c3239eaed37131f578d5ba92532357d64
vilayat571 <vilayat571@.com>
2022-11-25 23:23:02 -0700

    text generator

---
+++ script.js
@@ -0,0 +1,21 @@
+import data from './data.json' assert {type: "json"};
+
+let btn = document.querySelector('button');
+let inp = document.querySelector('input');
+let footer = document.getElementById("footer");
+
+let div = document.createElement('div');
+div.setAttribute('title','paragraph');
+
+btn.addEventListener('click',(e) => {
+
+    e.preventDefault();
+    let index = parseInt(inp.value) > 10 ? parseInt(inp.value) : parseInt(inp.value);
+    div.innerHTML = data.data.slice(0,index).map(item => {
+        return `<div>${item.text}</div>`
+    });
+
+});
+
+
+footer.appendChild(div);
